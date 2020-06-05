240165a08a95a06481d2e6c88a565ba4b3963c16
heliorrfreitas <heliorui.ramosfreitas@gus.pittstate.edu>
2020-06-05 18:32:40 -0500

    Add routes to backend, transactions and more

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,64 +1,15 @@
 import express from 'express';
-import knex from './database/connection';
+import PointsController from './controller/PointsController';
+import ItemsController from './controller/ItemsController';
 
 const routes = express.Router();
+const pointsController = new PointsController();
+const itemsController = new ItemsController();
 
-routes.get('/items', async (request, response) => {
-    const items = await knex('items').select('*');
+routes.get('/items', itemsController.index);
 
-    const serializedItems = items.map(item => {
-        return {
-            id: item.id,
-            title: item.title,
-            image_url: `http://localhost:3333/uploads/${item.image}`,
-        };
-    });
-
-    return response.json(serializedItems);
-});
-
-routes.post('/points', async (request, response) => {
-    const {
-        name, 
-        email,
-        whatsapp,
-        latitude,
-        longitude,
-        city,
-        uf,
-        items
-    } = request.body;
-
-    await knex('points').insert({
-        image: 'image-fake',
-        name, 
-        email,
-        whatsapp,
-        latitude,
-        longitude,
-        city,
-        uf,
-    });
-
-    return response.json({sucess: true});
-})
+routes.post('/points', pointsController.create);
+routes.get('/points', pointsController.index);
+routes.get('/points/:id', pointsController.show);
 
 export default routes; 
-
-/*
-{
-    "name": "Test 1", 
-    "email": "Test1@email.com",
-    "whatsapp": "1234556666",
-    "latitude":  -46.44123445,
-    "longitude": 96.49404940,
-    "city": "Sorocaba",
-    "uf": "SP",
-    items: [
-        1,
-        2,
-        3
-    ]
-}
-
-*/
